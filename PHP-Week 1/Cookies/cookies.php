420e0aa082d930b57fd13e7f9cc45d172f7416a2
Prashantd@ccube.com <prashant.d@ccube.com>
2025-06-18 17:37:35 +0530

    Commit new files

---
+++ PHP-Week 1/Cookies/cookies.php
@@ -0,0 +1,18 @@
+<?php
+// setcookie("fruit", "apple", time()+(86400));
+// setcookie("color","blue",time()+(86400));
+// if(isset($_COOKIE['fruit'])){
+//     echo "Current Cookies\t:" .$_COOKIE['fruit'];
+// } else {
+//     echo "No cookies set";
+// }
+
+// echo "<br/>";
+// if(isset($_COOKIE['color'])){
+//     echo "Current Color:\t" .$_COOKIE['color'];
+// } else {
+//     echo "No colour set";
+// }
+// echo "<br/>";
+// print_r($_COOKIE);
+?>
