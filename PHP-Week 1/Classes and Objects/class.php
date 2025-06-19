53418c7092890cf2f017adba65286cd46afc3822
Prashantd@ccube.com <prashant.d@ccube.com>
2025-06-19 16:46:27 +0530

    Add new files classes

--- PHP-Week 1/Classes and Objects/class.php
+++ PHP-Week 1/Classes and Objects/class.php
@@ -10,26 +10,26 @@
 ?>
 
 <?php
-class MathOperations {
-    function sum($a, $b){
-        return $a+$b;
-    }
+// class MathOperations {
+//     function sum($a, $b){
+//         return $a+$b;
+//     }
 
-    function mul($a, $b){
-        return $a*$b;
-    }
-}
+//     function mul($a, $b){
+//         return $a*$b;
+//     }
+// }
 
-$maths = new MathOperations();
-echo $maths->sum(20,30);
-echo "<br/>";
-echo $maths->sum(100,23); 
+// $maths = new MathOperations();
+// echo $maths->sum(20,30);
+// echo "<br/>";
+// echo $maths->sum(100,23); 
 
-$maths1 = new MathOperations();
-echo "<br/>";
-echo $maths1->sum(100,90);
+// $maths1 = new MathOperations();
+// echo "<br/>";
+// echo $maths1->sum(100,90);
 
-$maths2 = new MathOperations();
-echo "<br/>";
-echo $maths2->mul(3,8);
+// $maths2 = new MathOperations();
+// echo "<br/>";
+// echo $maths2->mul(3,8);
 ?>
